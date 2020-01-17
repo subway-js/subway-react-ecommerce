90e674ded8bc6f33e7101029edbcb875c3a8d93b
Danilo Rossi <contact@danilorossi.me>
2020-01-17 19:03:36 +0100

    Updates

---
+++ precache-manifest.459be3a392a3d0f759bf8659efcfe9ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f9096b3fe6342cbb9cce90235b777c",
+    "url": "/subway-react-ecommerce/index.html"
+  },
+  {
+    "revision": "ba4354cd9833c8708b9f",
+    "url": "/subway-react-ecommerce/static/js/2.cf8a5dee.chunk.js"
+  },
+  {
+    "revision": "0286b8a2f013faea43a755147f571c65",
+    "url": "/subway-react-ecommerce/static/js/2.cf8a5dee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ed509bb0bc22e56b600",
+    "url": "/subway-react-ecommerce/static/js/main.bce3248e.chunk.js"
+  },
+  {
+    "revision": "5058dbcbb0e0352bb4e1",
+    "url": "/subway-react-ecommerce/static/js/runtime-main.a1eb446f.js"
+  }
+]);
